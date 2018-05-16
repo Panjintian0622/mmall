dd4b8d880ea64c48b7ea48b2c58b926a6639e8b9
panjt <panjt@163.com>
2018-05-17 00:22:39 +0800

    收货地址模块

--- src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
+++ src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
@@ -1,5 +1,7 @@
 package com.mmall.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.google.common.collect.Maps;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.ShippingMapper;
@@ -8,6 +10,7 @@ import com.mmall.service.IShippingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -44,9 +47,25 @@ public class ShippingServiceImpl implements IShippingService{
         shipping.setUserId(userId);
         int rowCount =shippingMapper.updateByShipping(shipping);
         if(rowCount>0){
-            return ServerResponse.createBySuccess("添加地址成功");
+            return ServerResponse.createBySuccess("更新地址成功");
         }
-        return ServerResponse.createByErrorMessage("添加地址失败");
+        return ServerResponse.createByErrorMessage("更新地址失败");
     }
 
+    public ServerResponse<Shipping> select(Integer userId,Integer shippingId){
+        Shipping shipping = shippingMapper.selectByShippingIdUserId(userId,shippingId);
+        if(shipping == null){
+            return ServerResponse.createByErrorMessage("无法查询到该地址");
+        }
+        return ServerResponse.createBySuccess("查询地址成功",shipping);
+    }
+
+    public  ServerResponse<PageInfo> list(Integer userId,int pageNum,int pageSize){
+        PageHelper.startPage(pageNum,pageSize);
+        List<Shipping> shippingList =  shippingMapper.selectByUserId(userId);
+        PageInfo pageInfo = new PageInfo(shippingList);
+        return ServerResponse.createBySuccess(pageInfo);
+
+
+    }
 }
